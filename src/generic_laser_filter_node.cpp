e67f8a597040b890c38ae924bedc1b780b13e4b2
Jeremy Leibs <jleibs@gmail.com>
2009-09-25 03:56:57 +0000

    Cleaning up generic_laser_filter_node code since it is used as part of the laser_filters tutorial.
    git-svn-id: https://code.ros.org/svn/ros-pkg/stacks/laser_pipeline/trunk@24482 eb33c2ac-9c88-4c90-87e0-44a10359b0c3

--- src/generic_laser_filter_node.cpp
+++ src/generic_laser_filter_node.cpp
@@ -27,53 +27,59 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
+
 #include "ros/ros.h"
 #include "sensor_msgs/LaserScan.h"
 #include <message_filters/subscriber.h>
 #include "tf/message_filter.h"
-
 #include <tf/transform_listener.h>
-
 #include "filters/filter_chain.h"
 
-
-class GenericLaserScanFilterNode 
+class GenericLaserScanFilterNode
 {
-public:
-  GenericLaserScanFilterNode() 
-    :  filter_chain_("sensor_msgs::LaserScan")
-    , scan_sub_(nh_, "scan_in", 50)
-						  
-  {
-    ros::NodeHandle local_nh("~");
-    output_pub_ = local_nh.advertise<sensor_msgs::LaserScan>("output", 1000);
-    tf_filter_ = new tf::MessageFilter<sensor_msgs::LaserScan>(scan_sub_, tf_, "base_link", 50);
-    tf_filter_->registerCallback(boost::bind(&GenericLaserScanFilterNode::callback, this, _1));
-    tf_filter_->setTolerance(ros::Duration(0.03));
-
-    filter_chain_.configure("~");
-  }
-
-  ~GenericLaserScanFilterNode(){
-    delete tf_filter_;
-  }
-
-  void callback(const sensor_msgs::LaserScanConstPtr& msg_in)
-  {
-    filter_chain_.update (*msg_in, msg_);
-    output_pub_.publish(msg_);
-  }
-
 protected:
-  filters::FilterChain<sensor_msgs::LaserScan> filter_chain_;
+  // Our NodeHandle
   ros::NodeHandle nh_;
+
+  // Components for tf::MessageFilter
   tf::TransformListener tf_;
   message_filters::Subscriber<sensor_msgs::LaserScan> scan_sub_;
-  tf::MessageFilter<sensor_msgs::LaserScan>* tf_filter_;
-  ros::Publisher output_pub_;
+  tf::MessageFilter<sensor_msgs::LaserScan> tf_filter_;
+
+  // Filter Chain
+  filters::FilterChain<sensor_msgs::LaserScan> filter_chain_;
+
+  // Components for publishing
   sensor_msgs::LaserScan msg_;
+  ros::Publisher output_pub_;
 
-
+public:
+  // Constructor
+  GenericLaserScanFilterNode() :
+    scan_sub_(nh_, "scan_in", 50),
+    tf_filter_(scan_sub_, tf_, "base_link", 50),
+    filter_chain_("sensor_msgs::LaserScan")
+  {
+    // Configure filter chain
+    filter_chain_.configure("~");
+    
+    // Setup tf::MessageFilter for input
+    tf_filter_.registerCallback(boost::bind(&GenericLaserScanFilterNode::callback, this, _1));
+    tf_filter_.setTolerance(ros::Duration(0.03));
+    
+    // Advertise output
+    output_pub_ = nh_.advertise<sensor_msgs::LaserScan>("output", 1000);
+  }
+  
+  // Callback
+  void callback(const sensor_msgs::LaserScan::ConstPtr& msg_in)
+  {
+    // Run the filter chain
+    filter_chain_.update (*msg_in, msg_);
+    
+    // Publish the output
+    output_pub_.publish(msg_);
+  }
 };
 
 int main(int argc, char **argv)
@@ -85,4 +91,3 @@ int main(int argc, char **argv)
   
   return 0;
 }
-
