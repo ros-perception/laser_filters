11729c31dd62931583bb86022130b78d66852b40
Atsushi Watanabe <atsushi.w@ieee.org>
2018-01-30 11:08:06 +0900

    Apply ROS recommended indent style to ScanShadowsFilter

--- include/laser_filters/scan_shadows_filter.h
+++ include/laser_filters/scan_shadows_filter.h
@@ -43,25 +43,21 @@
 #include <sensor_msgs/LaserScan.h>
 #include <angles/angles.h>
 
-namespace laser_filters{
-
+namespace laser_filters
+{
 /** @b ScanShadowsFilter is a simple filter that filters shadow points in a laser scan line 
  */
 
 class ScanShadowsFilter : public filters::FilterBase<sensor_msgs::LaserScan>
 {
 public:
-
-  double laser_max_range_;           // Used in laser scan projection
-  double min_angle_, max_angle_;          // Filter angle threshold
+  double laser_max_range_;        // Used in laser scan projection
+  double min_angle_, max_angle_;  // Filter angle threshold
   int window_, neighbors_;
-    
 
   ////////////////////////////////////////////////////////////////////////////////
-  ScanShadowsFilter () 
+  ScanShadowsFilter()
   {
-
-
   }
 
   /**@b Configure the filter from XML */
@@ -82,7 +78,7 @@ public:
       ROS_ERROR("Error: ShadowsFilter was not given window.\n");
       return false;
     }
-    neighbors_ = 0;//default value
+    neighbors_ = 0;  // default value
     if (!filters::FilterBase<sensor_msgs::LaserScan>::getParam(std::string("neighbors"), neighbors_))
     {
       ROS_INFO("Error: ShadowsFilter was not given neighbors.\n");
@@ -92,7 +88,9 @@ public:
   }
 
   ////////////////////////////////////////////////////////////////////////////////
-  virtual ~ScanShadowsFilter () { }
+  virtual ~ScanShadowsFilter()
+  {
+  }
 
   /** @brief calculate the perpendicular angle at the end of r1 to get to r2 
    * See http://en.wikipedia.org/wiki/Law_of_cosines */
@@ -100,7 +98,6 @@ public:
   {
     return atan2(r2 * sin(included_angle), r1 - r2 * cos(included_angle));
   }
-
 
   ////////////////////////////////////////////////////////////////////////////////
   /** \brief Filter shadow points based on 3 global parameters: min_angle, max_angle
@@ -112,44 +109,43 @@ public:
    */
   bool update(const sensor_msgs::LaserScan& scan_in, sensor_msgs::LaserScan& scan_out)
   {
-    //copy across all data first
+    // copy across all data first
     scan_out = scan_in;
 
     std::set<int> indices_to_delete;
     // For each point in the current line scan
-    for (unsigned int i = 0; i < scan_in.ranges.size (); i++)
+    for (unsigned int i = 0; i < scan_in.ranges.size(); i++)
     {
       for (int y = -window_; y < window_ + 1; y++)
       {
         int j = i + y;
-        if ( j < 0 || j >= (int)scan_in.ranges.size () || (int)i == j ){ // Out of scan bounds or itself
+        if (j < 0 || j >= (int)scan_in.ranges.size() || (int)i == j)
+        {  // Out of scan bounds or itself
           continue;
-	}
-
-        double angle = abs(angles::to_degrees(getAngleWithViewpoint (scan_in.ranges[i],scan_in.ranges[j], y * scan_in.angle_increment)));
-        if (angle < min_angle_ || angle > max_angle_) 
-        {
-	  for (int index = std::max<int>(i - neighbors_, 0); index <= std::min<int>(i+neighbors_, (int)scan_in.ranges.size()-1); index++)
-	    {
-	      if (scan_in.ranges[i] < scan_in.ranges[index]) // delete neighbor if they are farther away (note not self)
-		indices_to_delete.insert(index);
-	    }
         }
 
+        double angle = abs(angles::to_degrees(getAngleWithViewpoint(scan_in.ranges[i], scan_in.ranges[j], y * scan_in.angle_increment)));
+        if (angle < min_angle_ || angle > max_angle_)
+        {
+          for (int index = std::max<int>(i - neighbors_, 0); index <= std::min<int>(i + neighbors_, (int)scan_in.ranges.size() - 1); index++)
+          {
+            if (scan_in.ranges[i] < scan_in.ranges[index])  // delete neighbor if they are farther away (note not self)
+              indices_to_delete.insert(index);
+          }
+        }
       }
     }
 
     ROS_DEBUG("ScanShadowsFilter removing %d Points from scan with min angle: %.2f, max angle: %.2f, neighbors: %d, and window: %d", (int)indices_to_delete.size(), min_angle_, max_angle_, neighbors_, window_);
-    for ( std::set<int>::iterator it = indices_to_delete.begin(); it != indices_to_delete.end(); ++it)
-      {
-          scan_out.ranges[*it] = std::numeric_limits<float>::quiet_NaN();  // Failed test to set the ranges to invalid value
-      }
+    for (std::set<int>::iterator it = indices_to_delete.begin(); it != indices_to_delete.end(); ++it)
+    {
+      scan_out.ranges[*it] = std::numeric_limits<float>::quiet_NaN();  // Failed test to set the ranges to invalid value
+    }
     return true;
   }
 
   ////////////////////////////////////////////////////////////////////////////////
-
-} ;
+};
 }
 
-#endif //LASER_SCAN_SHADOWS_FILTER_H
+#endif  // LASER_SCAN_SHADOWS_FILTER_H
