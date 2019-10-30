876ffb2fc8ec8ffdad2cc532a79699a63ffb7779
Patrick Lascombe <patrick.lascombe@wyca.fr>
2019-10-30 09:47:29 +0100

    Renaming the nodelet to a more explicit name

---
+++ src/scan_to_cloud_filter_chain_nodelet.cpp
@@ -0,0 +1,211 @@
+/*
+ * Copyright (c) 2008 Radu Bogdan Rusu <rusu@cs.tum.edu>
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $Id: scan_shadows_filter.cpp,v 1.0 2008/12/04 12:00:00 rusu Exp $
+ *
+ */
+
+/*
+\author Radu Bogdan Rusu <rusu@cs.tum.edu>
+
+
+ */
+
+#include <laser_filters/scan_to_cloud_filter_chain_nodelet.h>
+#include <pluginlib/class_list_macros.hpp>
+
+PLUGINLIB_EXPORT_CLASS(laser_filters::ScanToCloudFilterChain, nodelet::Nodelet)
+
+
+/** @b ScanShadowsFilter is a simple node that filters shadow points in a laser scan line and publishes the results in a cloud.
+ */
+namespace laser_filters {
+
+  ////////////////////////////////////////////////////////////////////////////////
+  void ScanToCloudFilterChain::onInit () {
+    laser_max_range_ = DBL_MAX;
+    nh = getNodeHandle();
+    private_nh = getPrivateNodeHandle();
+    filter_ = new tf::MessageFilter<sensor_msgs::LaserScan>(tf_, "", 50);
+    cloud_filter_chain_ = new filters::FilterChain<sensor_msgs::PointCloud2>("sensor_msgs::PointCloud2");
+    scan_filter_chain_ = new filters::FilterChain<sensor_msgs::LaserScan>("sensor_msgs::LaserScan");
+
+
+    private_nh.param("high_fidelity", high_fidelity_, false);
+    private_nh.param("notifier_tolerance", tf_tolerance_, 0.03);
+    private_nh.param("target_frame", target_frame_, std::string ("base_link"));
+
+    // DEPRECATED with default value
+    using_default_target_frame_deprecated_ = !private_nh.hasParam("target_frame");
+
+    // DEPRECATED
+    using_scan_topic_deprecated_ = private_nh.hasParam("scan_topic");
+    using_cloud_topic_deprecated_ = private_nh.hasParam("cloud_topic");
+    using_laser_max_range_deprecated_ = private_nh.hasParam("laser_max_range");
+    using_filter_window_deprecated_ = private_nh.hasParam("filter_window");
+    using_cloud_filters_deprecated_ = private_nh.hasParam("cloud_filters/filter_chain");
+    using_scan_filters_deprecated_ = private_nh.hasParam("scan_filters/filter_chain");
+    using_cloud_filters_wrong_deprecated_ = private_nh.hasParam("cloud_filters/cloud_filter_chain");
+    using_scan_filters_wrong_deprecated_ = private_nh.hasParam("scan_filters/scan_filter_chain");
+
+
+    private_nh.param("filter_window", window_, 2);
+    private_nh.param("laser_max_range", laser_max_range_, DBL_MAX);
+    private_nh.param("scan_topic", scan_topic_, std::string("tilt_scan"));
+    private_nh.param("cloud_topic", cloud_topic_, std::string("tilt_laser_cloud_filtered"));
+    private_nh.param("incident_angle_correction", incident_angle_correction_, true);
+
+    filter_->setTargetFrame(target_frame_);
+    filter_->registerCallback(boost::bind(&ScanToCloudFilterChain::scanCallback, this, _1));
+    filter_->setTolerance(ros::Duration(tf_tolerance_));
+
+    if (using_scan_topic_deprecated_)
+      sub_.subscribe(nh, scan_topic_, 50);
+    else
+      sub_.subscribe(nh, "scan", 50);
+
+    filter_->connectInput(sub_);
+
+    if (using_cloud_topic_deprecated_)
+      cloud_pub_ = nh.advertise<sensor_msgs::PointCloud2> (cloud_topic_, 10);
+    else
+      cloud_pub_ = nh.advertise<sensor_msgs::PointCloud2> ("cloud_filtered", 10);
+
+    std::string cloud_filter_xml;
+
+    if (using_cloud_filters_deprecated_)
+      cloud_filter_chain_->configure("cloud_filters/filter_chain", private_nh);
+    else if (using_cloud_filters_wrong_deprecated_)
+      cloud_filter_chain_->configure("cloud_filters/cloud_filter_chain", private_nh);
+    else
+      cloud_filter_chain_->configure("cloud_filter_chain", private_nh);
+
+    if (using_scan_filters_deprecated_)
+      scan_filter_chain_->configure("scan_filter/filter_chain", private_nh);
+    else if (using_scan_filters_wrong_deprecated_)
+      scan_filter_chain_->configure("scan_filters/scan_filter_chain", private_nh);
+    else
+      scan_filter_chain_->configure("scan_filter_chain", private_nh);
+
+    deprecation_timer_ = nh.createTimer(ros::Duration(5.0), boost::bind(&ScanToCloudFilterChain::deprecation_warn, this, _1));
+  }
+
+  ScanToCloudFilterChain::ScanToCloudFilterChain() {
+
+  }
+
+  ScanToCloudFilterChain::~ScanToCloudFilterChain() {
+
+  }
+
+  // We use a deprecation warning on a timer to avoid warnings getting lost in the noise
+  void ScanToCloudFilterChain::deprecation_warn(const ros::TimerEvent& e)
+  {
+    if (using_scan_topic_deprecated_)
+      ROS_WARN("Use of '~scan_topic' parameter in scan_to_cloud_filter_chain has been deprecated.");
+
+    if (using_cloud_topic_deprecated_)
+      ROS_WARN("Use of '~cloud_topic' parameter in scan_to_cloud_filter_chain has been deprecated.");
+
+    if (using_laser_max_range_deprecated_)
+      ROS_WARN("Use of '~laser_max_range' parameter in scan_to_cloud_filter_chain has been deprecated.");
+
+    if (using_filter_window_deprecated_)
+      ROS_WARN("Use of '~filter_window' parameter in scan_to_cloud_filter_chain has been deprecated.");
+
+    if (using_default_target_frame_deprecated_)
+      ROS_WARN("Use of default '~target_frame' parameter in scan_to_cloud_filter_chain has been deprecated.  Default currently set to 'base_link' please set explicitly as appropriate.");
+
+    if (using_cloud_filters_deprecated_)
+      ROS_WARN("Use of '~cloud_filters/filter_chain' parameter in scan_to_cloud_filter_chain has been deprecated.  Replace with '~cloud_filter_chain'");
+
+    if (using_scan_filters_deprecated_)
+      ROS_WARN("Use of '~scan_filters/filter_chain' parameter in scan_to_cloud_filter_chain has been deprecated.  Replace with '~scan_filter_chain'");
+
+    if (using_cloud_filters_wrong_deprecated_)
+      ROS_WARN("Use of '~cloud_filters/cloud_filter_chain' parameter in scan_to_cloud_filter_chain is incorrect.  Please Replace with '~cloud_filter_chain'");
+
+    if (using_scan_filters_wrong_deprecated_)
+      ROS_WARN("Use of '~scan_filters/scan_filter_chain' parameter in scan_to_scan_filter_chain is incorrect.  Please Replace with '~scan_filter_chain'");
+
+  }
+
+
+  ////////////////////////////////////////////////////////////////////////////////
+  void ScanToCloudFilterChain::scanCallback (const sensor_msgs::LaserScan::ConstPtr& scan_msg)
+  {
+    //    sensor_msgs::LaserScan scan_msg = *scan_in;
+
+    sensor_msgs::LaserScan filtered_scan;
+    scan_filter_chain_->update (*scan_msg, filtered_scan);
+
+    // Project laser into point cloud
+    sensor_msgs::PointCloud2 scan_cloud;
+
+    //\TODO CLEAN UP HACK 
+    // This is a trial at correcting for incident angles.  It makes many assumptions that do not generalise
+    if(incident_angle_correction_)
+    {
+      for (unsigned int i = 0; i < filtered_scan.ranges.size(); i++)
+      {
+        double angle = filtered_scan.angle_min + i * filtered_scan.angle_increment;
+        filtered_scan.ranges[i] = filtered_scan.ranges[i] + 0.03 * exp(-fabs(sin(angle)));
+      }
+    }
+
+    // Transform into a PointCloud message
+    int mask = laser_geometry::channel_option::Intensity |
+      laser_geometry::channel_option::Distance |
+      laser_geometry::channel_option::Index |
+      laser_geometry::channel_option::Timestamp;
+      
+    if (high_fidelity_)
+    {
+      try
+      {
+        projector_.transformLaserScanToPointCloud (target_frame_, filtered_scan, scan_cloud, tf_, mask);
+      }
+      catch (tf::TransformException &ex)
+      {
+        ROS_WARN("High fidelity enabled, but TF returned a transform exception to frame %s: %s", target_frame_.c_str (), ex.what ());
+        return;
+        //projector_.projectLaser (filtered_scan, scan_cloud, laser_max_range_, preservative_, mask);
+      }
+    }
+    else
+    {
+      projector_.transformLaserScanToPointCloud(target_frame_, filtered_scan, scan_cloud, tf_, laser_max_range_, mask);
+    }
+      
+    sensor_msgs::PointCloud2 filtered_cloud;
+    cloud_filter_chain_->update (scan_cloud, filtered_cloud);
+
+    cloud_pub_.publish(filtered_cloud);
+  }
+
+}
+
+
---
+++ src/scan_to_scan_filter_chain_nodelet.cpp
@@ -0,0 +1,119 @@
+/*
+ * Copyright (c) 2008, Willow Garage, Inc.
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Willow Garage, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+#include <pluginlib/class_list_macros.h>
+#include "laser_filters/scan_to_scan_filter_chain_nodelet.h"
+
+PLUGINLIB_EXPORT_CLASS(laser_filters::ScanToScanFilterChain, nodelet::Nodelet)
+
+namespace laser_filters {
+
+// Constructor
+  void ScanToScanFilterChain::onInit()
+
+    /*scan_sub_(nh_, "scan", 50),
+    tf_(NULL),
+    tf_filter_(NULL),
+    filter_chain_("sensor_msgs::LaserScan")*/
+  {
+    nh_= getNodeHandle();
+    private_nh_ = getPrivateNodeHandle();
+    scan_sub_.subscribe(nh_, "scan", 50);
+
+    filter_chain_ = new filters::FilterChain<sensor_msgs::LaserScan>("sensor_msgs::LaserScan");
+    private_nh_.param("filter_chain", using_filter_chain_deprecated_);
+
+    if (using_filter_chain_deprecated_) {
+      filter_chain_->configure("filter_chain", private_nh_);
+    } else {
+      filter_chain_->configure("scan_filter_chain", private_nh_);
+    }
+
+    std::string tf_message_filter_target_frame;
+
+    if (private_nh_.hasParam("tf_message_filter_target_frame"))
+    {
+      private_nh_.getParam("tf_message_filter_target_frame", tf_message_filter_target_frame);
+
+      private_nh_.param("tf_message_filter_tolerance", tf_filter_tolerance_, 0.03);
+
+      tf_ = new tf::TransformListener();
+      tf_filter_ = new tf::MessageFilter<sensor_msgs::LaserScan>(scan_sub_, *tf_, "", 50);
+      tf_filter_->setTargetFrame(tf_message_filter_target_frame);
+      tf_filter_->setTolerance(ros::Duration(tf_filter_tolerance_));
+
+      // Setup tf::MessageFilter generates callback
+      tf_filter_->registerCallback(boost::bind(&ScanToScanFilterChain::callback, this, _1));
+    }
+    else 
+    {
+      // Pass through if no tf_message_filter_target_frame
+      scan_sub_.registerCallback(boost::bind(&ScanToScanFilterChain::callback, this, _1));
+    }
+    
+    // Advertise output
+    output_pub_ = nh_.advertise<sensor_msgs::LaserScan>("scan_filtered", 1000);
+
+    // Set up deprecation printout
+    deprecation_timer_ = nh_.createTimer(ros::Duration(5.0), boost::bind(&ScanToScanFilterChain::deprecation_warn, this, _1));
+  }
+
+  ScanToScanFilterChain::ScanToScanFilterChain()
+  {
+
+  }
+  // Destructor
+  ScanToScanFilterChain::~ScanToScanFilterChain()
+  {
+    if (tf_filter_)
+      delete tf_filter_;
+    if (tf_)
+      delete tf_;
+  }
+  
+  // Deprecation warning callback
+  void ScanToScanFilterChain::deprecation_warn(const ros::TimerEvent& e)
+  {
+    if (using_filter_chain_deprecated_)
+      ROS_WARN("Use of '~filter_chain' parameter in scan_to_scan_filter_chain has been deprecated. Please replace with '~scan_filter_chain'.");
+  }
+
+  // Callback
+  void ScanToScanFilterChain::callback(const sensor_msgs::LaserScan::ConstPtr& msg_in)
+  {
+    // Run the filter chain
+    if (filter_chain_->update(*msg_in, msg_))
+    {
+      //only publish result if filter succeeded
+      output_pub_.publish(msg_);
+    }
+  }
+};
+
