cf27b3654f5e998d21aa6aeb72e37c8663ce422b
hang <thomas_hang0000@hotmail.com>
2020-11-05 11:29:31 +0800

    fixed examples yamls with ros2 parameter syntax, translated launch xml -> py

---
+++ examples/range_filter_example.launch.py
@@ -0,0 +1,18 @@
+from launch import LaunchDescription
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package="laser_filters",
+            executable="scan_to_scan_filter_chain",
+            parameters=[
+                PathJoinSubstitution([
+                    get_package_share_directory("laser_filters"),
+                    "examples", "range_filter_example.yaml",
+                ])],
+        )
+    ])
