5161f2d23f9875e7a26357062b089a247b06ff13
Erwin Bonsma <erwin.bonsma@topic.nl>
2021-11-09 10:18:56 +0100

    Fix to CI set-up (a workaround)

--- ci.sh
+++ ci.sh
@@ -9,6 +9,11 @@ BUILD_DIR=build
 mkdir -p ${BUILD_DIR}
 (cd ${BUILD_DIR} && cmake .. -DCATKIN_ENABLE_TESTING=1)
 
+# Workaround. Invoke cmake twice to ensure that build/catkin_generated/setup_cached.sh adds
+# build/devel/lib to LD_LIBRARY_PATH (so that tests use the built laser_filters library instead
+# the of installed one).
+(cd ${BUILD_DIR} && cmake .. -DCATKIN_ENABLE_TESTING=1)
+
 # Build.
 make -C ${BUILD_DIR}
 
