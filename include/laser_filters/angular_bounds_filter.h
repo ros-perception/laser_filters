8d24bf2b9ce7f724bf7669cce1a85851392f8385
JosefGst <josefgstoettner@live.at>
2023-11-01 15:39:36 +0800

    change to rclcpp Time

--- include/laser_filters/angular_bounds_filter.h
+++ include/laser_filters/angular_bounds_filter.h
@@ -71,8 +71,7 @@ namespace laser_filters
 
         double start_angle = input_scan.angle_min;
         double current_angle = input_scan.angle_min;
-        builtin_interfaces::msg::Time start_time = input_scan.header.stamp;
-        rclcpp::Duration d = rclcpp::Duration(0, 0);
+        rclcpp::Time start_time = input_scan.header.stamp;
         unsigned int count = 0;
         //loop through the scan and truncate the beginning and the end of the scan as necessary
         for(unsigned int i = 0; i < input_scan.ranges.size(); ++i){
@@ -80,7 +79,7 @@ namespace laser_filters
           if(start_angle < lower_angle_){
             start_angle += input_scan.angle_increment;
             current_angle += input_scan.angle_increment;
-            d = d + rclcpp::Duration::from_seconds(input_scan.time_increment);
+            start_time += rclcpp::Duration::from_seconds(input_scan.time_increment);
           }
           else{
             filtered_scan.ranges[count] = input_scan.ranges[i];
@@ -100,8 +99,6 @@ namespace laser_filters
 
           }
         }
-        start_time.sec += d.seconds();
-        start_time.nanosec += d.nanoseconds();
 
         //make sure to set all the needed fields on the filtered scan
         filtered_scan.header.frame_id = input_scan.header.frame_id;
