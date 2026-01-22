73dfa593888097a781c6e1e6ec41ecc045c318b1
daniel-zellmann-lowpad <daniel.zellmann@lowpad.com>
2026-01-22 07:16:49 +0100

    Update polygon_filter.h

--- include/laser_filters/polygon_filter.h
+++ include/laser_filters/polygon_filter.h
@@ -52,7 +52,7 @@
 #include <geometry_msgs/msg/polygon.hpp>
 #include <geometry_msgs/msg/polygon_stamped.hpp>
 
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #include <tf2_ros/buffer.h>
 #include <boost/thread.hpp>
 #include <rclcpp/rclcpp.hpp>
