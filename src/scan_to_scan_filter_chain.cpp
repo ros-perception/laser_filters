51d7c3280e1e1f9a16e4d20657bd20cb5b3d82c0
William Woodall <wjwwood@google.com>
2026-09-17 23:05:56 +0000

    Update CreateTimerROS constructor call for NodeInterfaces API
    Signed-off-by: William Woodall <wjwwood@google.com>

--- src/scan_to_scan_filter_chain.cpp
+++ src/scan_to_scan_filter_chain.cpp
@@ -73,9 +73,7 @@ ScanToScanFilterChain::ScanToScanFilterChain(
   this->get_parameter("scan_filtered_history_depth", scan_filtered_history_depth_);
 
   if (!tf_message_filter_target_frame_.empty()) {
-    auto timer_interface = std::make_shared<tf2_ros::CreateTimerROS>(
-      get_node_base_interface(),
-      get_node_timers_interface());
+    auto timer_interface = std::make_shared<tf2_ros::CreateTimerROS>(*this);
     buffer_.setCreateTimerInterface(std::move(timer_interface));
     tf_.reset(new tf2_ros::TransformListener(buffer_));
     tf_filter_.reset(
